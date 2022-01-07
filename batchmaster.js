3d3b12b8a5860aa5be357287070876fffdffacc5
Gleichlucas <lucas.kunz94@gmail.com>
2022-01-07 02:51:11 +0100

    semiworking batchmaster- are the statingtimes correct?

--- batchmaster.js
+++ batchmaster.js
@@ -18,7 +18,7 @@
     
     let target = ns.args[0] ?? 'n00dles';
 
-    ns.disableLog("ALL");
+    //ns.disableLog("ALL");
     let minSec = ns.getServerMinSecurityLevel(target);
     let nowSec = ns.getServerSecurityLevel(target);
 
@@ -27,34 +27,44 @@
 
     let freeRam = ns.getServerMaxRam(ns.getHostname()) - ns.getServerUsedRam(ns.getHostname());
     let threadRam = 1.75;
-    ns.exec('BitBurner-scripts/prime.js',ns.getHostname(), 1, target);
 
-    ns.enableLog("print");
+    //ns.enableLog("print");
 
     // one formulas are unlocked hackExp(server, player)
     let playerHackLvl = ns.getHackingLevel();
-    setTimeout(callWeaken,); 
-    let batchTime = 100;
+    let batchTime = 50;
     // these change everytime the playerhacklvl changes
     let weakenTime = ns.getWeakenTime(target);
     let growTime = ns.getGrowTime(target);
     let hackTime = ns.getHackTime(target);
     let startTimes = [batchTime, batchTime +(weakenTime-growTime),3*batchTime, weakenTime - hackTime - batchTime];  //w, w ,g h
 
-    for (let cycle = 0; cycle < 100; cycle++){
-        setTimeout(callWeaken, startTimes[0]+ cycle*batchTime,target , 1); 
-        setTimeout(callWeaken, startTimes[1]+ cycle*batchTime,target , 1); 
-        setTimeout(callGrow, startTimes[2]+ cycle*batchTime,target , 1); 
-        setTimeout(callHack, startTimes[3]+ cycle*batchTime,target , 1); 
+    let maxLoop = Math.floor(freeRam/ (1.75*4));
+    maxLoop = 1;
+    ns.tprint("Batches: ",maxLoop);
+    for (let cycle = 0; cycle < maxLoop; cycle++){
+        setTimeout(callWeaken, startTimes[0]+ cycle*batchTime,target , 1, ns);
+        ns.print("callWeaken, starttime: ", startTimes[0]+ cycle*batchTime);
+
+        setTimeout(callWeaken, startTimes[1]+ cycle*batchTime,target , 1, ns);
+        ns.print("callWeaken, starttime: ", startTimes[1]+ cycle*batchTime);
+
+        setTimeout(callGrow, startTimes[2]+ cycle*batchTime,target , 1, ns); 
+        ns.print("callGrow, starttime: ", startTimes[2]+ cycle*batchTime);
+
+        setTimeout(callHack, startTimes[3]+ cycle*batchTime,target , 1, ns);
+        ns.print("callHack, starttime: ", startTimes[3]+ cycle*batchTime);
     }
+    setTimeout(callWeaken, startTimes[0]+ batchTime*maxLoop,target , 1, ns);
+    await ns.asleep(3*maxLoop*weakenTime);
 }
 
-function callWeaken(target, threads){
+function callWeaken(target, threads, ns){
     ns.exec('BitBurner-scripts/weaken.js',ns.getHostname(), threads, target);
 }
-function callGrow(target, threads){
+function callGrow(target, threads, ns){
     ns.exec('BitBurner-scripts/grow.js',ns.getHostname(), threads, target);
 }
-function callHack(target, threads){
+function callHack(target, threads, ns){
     ns.exec('BitBurner-scripts/hack.js',ns.getHostname(), threads, target);
 }
