f61ce4396f462fb4af18f32068eb94562a4e8fb1
Gleichlucas <lucas.kunz94@gmail.com>
2022-01-13 18:57:39 +0100

    made and andimaker, lel

---
+++ gang/recruit.js
@@ -0,0 +1,19 @@
+/**
+ * 
+ * recruit all andis and train them
+ */
+/** @param @param {import("../.").NS } ns **/
+export async function main(ns) {
+
+    let memberList = ns.gang.getMemberNames();
+    let andiNbr = 0;
+    while (memberList.includes('andi-'+ andiNbr)){
+        andiNbr++;
+    }
+    
+    while (ns.gang.canRecruitMember()){
+        let memberName = 'andi-' + andiNbr++;
+        ns.gang.setMemberTask(memberName, "Train Combat");
+    }
+    ns.tprint("number of andis in gang is now: ",andiNbr);
+}
