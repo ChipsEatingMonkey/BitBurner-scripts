dd9258bbcd619d860f4ac982e569791ad31937bd
Gleichlucas <lucas.kunz94@gmail.com>
2022-01-18 17:28:34 +0100

    changed folder of src files

--- copy.js
+++ copy.js
@@ -7,8 +7,8 @@ export async function main(ns) {
     
     let target = ns.args[0] ?? 'n00dles';
     let chadAll = ns.args[1] ?? 0;
-    let folder = '/BitBurner-scripts/';
-    let scripts = [folder+'weaken.js', folder+'grow.js', folder+'hack.js',folder+'prime.js', folder+'weakenloop.js'];
+    let folder = './';//'/BitBurner-scripts/';
+    let scripts = [folder+'weaken.js', folder+'grow.js', folder+'hack.js',folder+'prime.js'];
     if (chadAll == 1){
         for (let i = 0; i < 25; i++){
             await ns.scp(scripts, 'home', 'chad-'+i);
